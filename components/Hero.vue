dcec2d011354e0ea93b907c8ee3be5fb5e87e216
syakirrafaei <syakir.rafaei@gmail.com>
2020-12-29 16:14:51 +0800

    Add hero video background.

---
+++ components/Hero.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="relative">
+    <video
+      ref="myVideo"
+      width="100%"
+      class="object-cover shadow-lg relative z-0"
+      style="height: 100vh"
+      muted
+      autoplay
+      @ended="onEnd()"
+    >
+      <source src="~assets/videos/hero.mp4" type="video/mp4" />
+    </video>
+    <div class="absolute hero_section">
+      <div
+        class="flex flex-wrap flex-col px-6 text-center xl:text-left xl:px-20"
+      >
+        <div class="font-bold text-white text-2xl xl:text-3xl">
+          16+ YEARS OF PROVEN IT SOLUTIONS AND TRAININGS
+        </div>
+        <div class="font-bold text-white text-xl mt-10 mb-4">
+          <span class="text-lg xl:text-2xl xl:ml-10">149+</span> satisfied
+          clients.
+        </div>
+        <div class="font-bold text-white text-xl my-4">
+          <span class="text-lg xl:text-2xl xl:ml-40">68+</span> training
+          sessions.
+        </div>
+        <div class="font-bold text-white text-xl my-4">
+          <span class="text-lg xl:text-2xl xl:ml-64">46+</span> projects
+          delivered.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    onEnd() {
+      this.$refs.myVideo.currentTime = 9.5
+      this.$refs.myVideo.play()
+    },
+  },
+}
+</script>
+<style scoped>
+.hero_section {
+  top: 20%;
+  left: 0px;
+  right: 0px;
+}
+@media (min-width: 1280px) {
+  .hero_section {
+    top: 25%;
+    left: 50%;
+  }
+}
+</style>
