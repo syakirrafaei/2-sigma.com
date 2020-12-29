6bf626db2b358a91f3bd40c7b4fb395ef2f72fc2
syakirrafaei <syakir.rafaei@gmail.com>
2020-12-29 16:14:35 +0800

    Add header.

---
+++ components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="fixed hidden xl:flex xl:flex-wrap xl:flex-row w-full bg-transparent px-12 py-6 text-white font-semibold z-10"
+  >
+    <div class="flex w-2/5 text-center">
+      <div class="w-full">About Us</div>
+      <div class="w-full">Our Services</div>
+      <div class="w-full">Our Clients</div>
+    </div>
+    <div class="w-1/5 text-center">TWO SIGMA</div>
+    <div class="flex w-2/5 text-center">
+      <div class="w-full">Training Offered</div>
+      <div class="w-full">Contact Us</div>
+      <div class="w-full">Careers</div>
+    </div>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
+    <Header />
     <Nuxt />
   </div>
 </template>
 
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -24,32 +24,16 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+body::-webkit-scrollbar {
+  width: 0.5em;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+body::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
 }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+body::-webkit-scrollbar-thumb {
+  background-color: rgb(97, 36, 0);
+  outline: 0px;
 }
 </style>
