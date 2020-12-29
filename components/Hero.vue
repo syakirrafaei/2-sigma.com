adac203737d2b63f73a8bd75f5cf4cb8c8c1d6b8
syakirrafaei <syakir.rafaei@gmail.com>
2020-12-29 22:28:30 +0800

    Add darker llinear gradient to background.

--- components/Hero.vue
+++ components/Hero.vue
@@ -3,22 +3,24 @@
     <div
       class="hero_placeholder bg-orange-900 object-cover shadow-lg relative z-0 transition ease-in-out duration-700"
     ></div>
-    <div class="absolute hero_section">
+    <div class="absolute hero_section text-white font-bold">
       <div
         class="flex flex-wrap flex-col px-6 text-center xl:text-left xl:px-20"
       >
-        <div class="font-bold text-white text-2xl xl:text-3xl">
-          16+ YEARS OF PROVEN IT SOLUTIONS AND TRAININGS
+        <div class="xl:-ml-10">
+          <span class="text-2xl xl:text-3xl"
+            >16+ YEARS OF PROVEN IT SOLUTIONS AND TRAININGS</span
+          >
         </div>
-        <div class="font-bold text-white text-xl mt-10 mb-4">
+        <div class="mt-10 mb-4">
           <span class="text-lg xl:text-2xl xl:ml-10">149+</span> satisfied
           clients.
         </div>
-        <div class="font-bold text-white text-xl my-4">
+        <div class="my-4">
           <span class="text-lg xl:text-2xl xl:ml-40">68+</span> training
           sessions.
         </div>
-        <div class="font-bold text-white text-xl my-4">
+        <div class="my-4">
           <span class="text-lg xl:text-2xl xl:ml-64">46+</span> projects
           delivered.
         </div>
@@ -44,7 +46,12 @@ export default {
 .hero_placeholder {
   width: 100vw;
   height: 100vh;
-  background: linear-gradient(rgba(0, 0, 0, 0.459), rgba(184, 83, 1, 0.445)),
+  background: linear-gradient(
+      to right,
+      rgb(255, 255, 255, 0),
+      rgba(0, 0, 0, 0.76)
+    ),
+    linear-gradient(rgba(0, 0, 0, 0.459), rgba(184, 83, 1, 0.445)),
     url('~assets/images/hero.png');
   background-size: cover;
 }
