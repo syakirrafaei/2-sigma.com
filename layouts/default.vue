ac4dcf2daa222c151510e08f7d598483f9dd3963
syakirrafaei <syakir.rafaei@gmail.com>
2020-12-30 13:54:10 +0800

    CSS orange color.

--- layouts/default.vue
+++ layouts/default.vue
@@ -37,4 +37,21 @@ body::-webkit-scrollbar-thumb {
   background-color: rgb(97, 36, 0);
   outline: 0px;
 }
+
+.text-orange {
+  color: #bc5700;
+}
+.divider-orange {
+  background: transparent
+    radial-gradient(
+      closest-side at 50% 50%,
+      #dd8437 0%,
+      #ba6a23 25%,
+      #df7920 44%,
+      #e07313 70%,
+      #a0500a 85%,
+      #5e2c00 100%
+    )
+    0% 0% no-repeat padding-box;
+}
 </style>
