dba39f296d182f5ce094095ce0fbcf4879edfbfe
syakirrafaei <syakir.rafaei@gmail.com>
2020-12-30 13:53:59 +0800

    Services section.

---
+++ components/Services.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="h-screen w-full bg-black p-12">
+    <div class="inline-block">
+      <div class="text-3xl font-bold text-orange">
+        Supercharge your business.
+      </div>
+      <div class="block py-1 divider-orange opacity-75"></div>
+    </div>
+    <div class="flex flex-row flex-wrap text-white my-8">
+      <div class="w-full xl:w-3/5">
+        <p class="xl:pr-64 text-lg">
+          After over several years in business, Two Sigma Technologies has
+          served literally hundreds of satisfied clients.
+        </p>
+      </div>
+      <div class="w-full xl:w-2/5"></div>
+    </div>
+  </div>
+</template>
