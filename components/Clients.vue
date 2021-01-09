a24f48b42273dfa71d373afc506e58c309b210e5
syakirrafaei <syakir.rafaei@gmail.com>
2021-01-09 20:37:31 +0800

    Add clients section.

---
+++ components/Clients.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="h-screen w-full bg-section p-16">
+    <div class="inline-block">
+      <div class="text-3xl font-bold text-orange">Our Happy Clients.</div>
+      <div class="block py-1 divider-orange opacity-75"></div>
+    </div>
+    <div class="flex flex-row flex-wrap text-white my-8">
+      <div class="w-full xl:w-3/5">
+        <p class="xl:pr-64 text-lg">
+          After over several years in business, Two Sigma Technologies has
+          served literally hundreds of satisfied clients.
+        </p>
+        <div class="flex flex-row flex-wrap mt-16"></div>
+      </div>
+      <div class="w-full xl:w-2/5"></div>
+    </div>
+  </section>
+</template>
+<style scoped>
+.bg-section {
+  background: transparent linear-gradient(0deg, #9b4930 0%, #17141b 100%) 0% 0%
+    no-repeat padding-box;
+}
+</style>
