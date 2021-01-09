6f8b81557097b6978c824fb4d8f648149b791260
syakirrafaei <syakir.rafaei@gmail.com>
2021-01-09 20:37:37 +0800

    Add contact section.

---
+++ components/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="h-screen w-full bg-section p-16">
+    <div class="inline-block">
+      <div class="text-3xl font-bold text-white">Contact Us.</div>
+      <div class="block py-1 bg-white opacity-75"></div>
+    </div>
+    <div class="flex flex-row flex-wrap text-white my-8">
+      <div class="w-full xl:w-3/5">
+        <p class="xl:pr-64 text-lg">You can reach us at:</p>
+        <div class="flex flex-row flex-wrap mt-16"></div>
+      </div>
+      <div class="w-full xl:w-2/5"></div>
+    </div>
+  </section>
+</template>
+<style scoped>
+.bg-section {
+  background: transparent
+    linear-gradient(0deg, #9c513a 0%, #b16634 51%, #9b4930 100%) 0% 0% no-repeat
+    padding-box;
+}
+</style>
