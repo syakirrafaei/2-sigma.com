4b52f9e80c043f6e06badc771b12663a3d122f3a
syakirrafaei <syakir.rafaei@gmail.com>
2021-01-15 20:28:24 +0800

    Add nuxt-link and more pages.

--- components/Header.vue
+++ components/Header.vue
@@ -4,11 +4,13 @@
       class="absolute hidden xl:flex xl:flex-wrap xl:flex-row w-full bg-transparent px-12 py-6 text-white font-semibold z-10"
     >
       <div class="flex w-2/5 text-center">
-        <div class="w-full">About Us</div>
-        <div class="w-full">Our Services</div>
+        <nuxt-link to="/about" class="w-full">About Us</nuxt-link>
+        <nuxt-link to="/our-services" class="w-full">Our Services</nuxt-link>
         <div class="w-full">Our Clients</div>
       </div>
-      <div class="w-1/5 text-center">TWO SIGMA</div>
+      <nuxt-link to="/" class="w-1/5 text-center font-bold"
+        >TWO SIGMA</nuxt-link
+      >
       <div class="flex w-2/5 text-center">
         <div class="w-full">Training Offered</div>
         <div class="w-full">Contact Us</div>
@@ -17,12 +19,12 @@
     </div>
     <div class="xl:hidden fixed top-0 right-0 mt-6 mr-6" style="z-index: 99">
       <svg
-        @click="showMenu = !showMenu"
         class="w-10 h-10 text-white"
         fill="none"
         stroke="currentColor"
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
+        @click="showMenu = !showMenu"
       >
         <path
           v-if="showMenu === false"
@@ -49,7 +51,7 @@
         <div
           class="flex flex-col flex-wrap text-white text-center text-2xl font-semibold px-6 py-12 mt-20"
         >
-          <div class="mb-8">About Us</div>
+          <nuxt-link to="/about" class="mb-8">About Us</nuxt-link>
           <div class="mb-8">Our Services</div>
           <div class="mb-8">Our Clients</div>
           <div class="mb-8">Training Offered</div>
@@ -80,12 +82,3 @@ export default {
   },
 }
 </script>
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
-</style>
