68d10a3e7904cb082e4fab7b991ac1a1dd74c232
syakirrafaei <syakir.rafaei@gmail.com>
2021-01-22 12:22:03 +0800

    Remove whitespace

--- components/Clients.vue
+++ components/Clients.vue
@@ -6,7 +6,7 @@
     </div>
     <div class="flex flex-row flex-wrap text-white my-8">
       <div class="w-full xl:w-3/5">
-        <p class="xl:pr-64 text-lg ">
+        <p class="xl:pr-64 text-lg">
           After over several years in business, Two Sigma Technologies has
           served literally hundreds of satisfied clients.
         </p>
