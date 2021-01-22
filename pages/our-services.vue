2e803e40f7d9b31c7c9a33054fda046afe5399f0
syakirrafaei <syakir.rafaei@gmail.com>
2021-01-23 02:44:02 +0800

    UI for our services page.

--- pages/our-services.vue
+++ pages/our-services.vue
@@ -1,5 +1,126 @@
 <template>
   <div>
-    <Contact />
+    <section class="bg-about px-16">
+      <div class="flex flex-row flex-wrap text-white py-40">
+        <div class="w-full xl:w-1/2">
+          <div
+            class="w-full text-4xl text-center xl:text-left font-semibold text-white mb-2"
+          >
+            Our Services
+          </div>
+          <div class="mt-6 xl:mt-2 w-full text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum,
+            suscipit in voluptates veniam iure natus ullam quos facilis soluta
+            iusto quaerat nemo, quidem voluptate id temporibus eveniet quo aut
+            veritatis deserunt libero dolore quis nesciunt! Labore praesentium
+            nemo totam ratione?
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="min-h-screen w-full bg-section p-16">
+      <div class="inline-block">
+        <div class="text-3xl font-bold capitalize text-orange">
+          Our Area of Expertise
+        </div>
+        <div class="block py-1 divider-orange opacity-75"></div>
+      </div>
+      <div class="flex flex-row flex-wrap py-10">
+        <div class="xl:w-1/6">
+          <div class="w-16 md:w-32">
+            <div
+              class="h-16 w-16 md:h-32 md:w-32 rounded-full shadow-lg bg-gray-900 border border-orange-700"
+            >
+              <img
+                src="~/assets/images/training.svg"
+                alt="IT software training"
+                class="h-16 w-16 md:h-32 md:w-32 my-auto p-2 pb-4"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="xl:w-5/6 mb-8">
+          <h1 class="text-2xl text-gray-100 font-semibold">IT Training</h1>
+          <p class="xl:w-2/3 mt-2 text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            harum beatae reiciendis architecto ea nesciunt neque, sapiente
+            corporis delectus! Ex at facere voluptate suscipit perspiciatis
+            ullam expedita nobis praesentium aperiam?
+          </p>
+        </div>
+        <div class="text-right xl:w-5/6">
+          <h1 class="text-2xl text-gray-100 font-semibold">
+            Website Development and Design
+          </h1>
+          <p class="xl:w-2/3 ml-auto mt-2 text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            harum beatae reiciendis architecto ea nesciunt neque, sapiente
+            corporis delectus! Ex at facere voluptate suscipit perspiciatis
+            ullam expedita nobis praesentium aperiam?
+          </p>
+        </div>
+        <div class="xl:w-1/6">
+          <div class="ml-auto w-16 md:w-32">
+            <div
+              class="h-16 w-16 md:h-32 md:w-32 rounded-full shadow-xl bg-gray-900 border border-orange-700"
+            >
+              <img
+                src="~/assets/images/web-development.svg"
+                alt="web development"
+                class="h-16 w-16 md:h-32 md:w-32 my-auto mx-auto pb-4"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="xl:w-1/6">
+          <div class="w-16 md:w-32">
+            <div
+              class="h-16 w-16 md:h-32 md:w-32 rounded-full shadow-lg bg-gray-900 border border-orange-700"
+            >
+              <img
+                src="~/assets/images/mobile-development.svg"
+                alt="mobile development"
+                class="h-16 w-16 md:h-32 md:w-32 my-auto p-2 pb-4"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="xl:w-5/6 mb-8">
+          <h1 class="text-2xl text-gray-100 font-semibold">
+            Mobile Application
+          </h1>
+          <p class="xl:w-2/3 mt-2 text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            harum beatae reiciendis architecto ea nesciunt neque, sapiente
+            corporis delectus! Ex at facere voluptate suscipit perspiciatis
+            ullam expedita nobis praesentium aperiam?
+          </p>
+        </div>
+        <div class="text-right xl:w-5/6">
+          <h1 class="text-2xl text-gray-100 font-semibold">
+            IT Infrastructure and Hardware
+          </h1>
+          <p class="xl:w-2/3 ml-auto mt-2 text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            harum beatae reiciendis architecto ea nesciunt neque, sapiente
+            corporis delectus! Ex at facere voluptate suscipit perspiciatis
+            ullam expedita nobis praesentium aperiam?
+          </p>
+        </div>
+        <div class="xl:w-1/6">
+          <div class="ml-auto w-16 md:w-32">
+            <div
+              class="h-16 w-16 md:h-32 md:w-32 rounded-full shadow-xl bg-gray-900 border border-orange-700"
+            >
+              <img
+                src="~/assets/images/it_infrastructure.svg"
+                alt="IT infrastructure"
+                class="h-16 w-16 md:h-32 md:w-32 my-auto mx-auto pb-4"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
