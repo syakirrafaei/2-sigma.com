37700f53e3f0d7261c67ca566224a8bc0e20d892
RIDDUWAN <ridzwankmpk@gmail.com>
2021-01-23 00:33:44 +0800

    temporary templete for seo

---
+++ pages/contact-us.vue
@@ -0,0 +1,26 @@
+<template>
+<Services :contacts ="contacts"/>
+</template>
+<script>
+export default {
+  async asyncData({ $axios }) {
+
+    const contacts = await $axios.$get('/homepage')
+
+    return { contacts }
+  },
+  head() {
+    return {
+      title: this.service.contacts,
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.service.contacts_description,
+        },
+      ],
+    }
+  },
+}
+</script>
