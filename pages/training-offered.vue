e476b57b8f9e5f87b86b308ca17ea983e02ce8a9
ahmadhabibullahjohan <myhabib69@gmail.com>
2021-01-23 12:40:34 +0800

    training offered view

---
+++ pages/training-offered.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Services :traindata ="traindata"/>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ $axios }) {
+
+    const traindata = await $axios.$get('/homepage')
+
+    return { traindata }
+  },
+  head() {
+    return {
+      title: this.traindata.meta_title,
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.traindata.meta_description,
+        },
+      ],
+    }
+  },
+}
+</script>
